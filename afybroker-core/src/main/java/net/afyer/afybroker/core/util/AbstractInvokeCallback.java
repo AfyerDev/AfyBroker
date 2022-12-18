160c8dc98410b1039619bfeefe7cc60cb2b1d467
Nipuru <2789129195@qq.com>
2022-12-18 17:33:58 +0800

    update

---
+++ afybroker-core/src/main/java/net/afyer/afybroker/core/util/AbstractInvokeCallback.java
@@ -0,0 +1,24 @@
+package net.afyer.afybroker.core.util;
+
+import com.alipay.remoting.InvokeCallback;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author Nipuru
+ * @since 2022/12/18 17:32
+ */
+public abstract class AbstractInvokeCallback implements InvokeCallback {
+
+    @Override
+    public void onResponse(Object result) { }
+
+    @Override
+    public void onException(Throwable e) { }
+
+    @Override
+    public Executor getExecutor() {
+        return null;
+    }
+
+}
--- afybroker-server/src/main/java/net/afyer/afybroker/server/processor/ForwardingMessageWrapperBrokerProcessor.java
+++ afybroker-server/src/main/java/net/afyer/afybroker/server/processor/ForwardingMessageWrapperBrokerProcessor.java
@@ -2,18 +2,17 @@ package net.afyer.afybroker.server.processor;
 
 import com.alipay.remoting.AsyncContext;
 import com.alipay.remoting.BizContext;
-import com.alipay.remoting.InvokeCallback;
 import com.alipay.remoting.exception.RemotingException;
 import com.alipay.remoting.rpc.protocol.AsyncUserProcessor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import net.afyer.afybroker.core.message.ForwardingMessageWrapper;
+import net.afyer.afybroker.core.util.AbstractInvokeCallback;
 import net.afyer.afybroker.server.BrokerServer;
 import net.afyer.afybroker.server.aware.BrokerServerAware;
 import net.afyer.afybroker.server.proxy.BrokerClientProxy;
 
 import java.io.Serializable;
-import java.util.concurrent.Executor;
 
 /**
  * @author Nipuru
@@ -38,7 +37,7 @@ public class ForwardingMessageWrapperBrokerProcessor extends AsyncUserProcessor<
         //如果有回调则异步执行 否则直接oneway
          if (request.isHasResponse()) {
              try {
-                 target.invokeWithCallback(message, new InvokeCallback() {
+                 target.invokeWithCallback(message, new AbstractInvokeCallback() {
                      @Override
                      public void onResponse(Object result) {
                          asyncCtx.sendResponse(result);
@@ -47,11 +46,6 @@ public class ForwardingMessageWrapperBrokerProcessor extends AsyncUserProcessor<
                      @Override
                      public void onException(Throwable e) {
                          log.error(e.getMessage(), e);
-                     }
-
-                     @Override
-                     public Executor getExecutor() {
-                         return null;
                      }
                  });
              } catch (RemotingException | InterruptedException e) {
